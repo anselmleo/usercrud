f506ba4edbab304b04b92128b792298fc1128ecc
Anselm <anselmleo@gmail.com>
2024-08-02 00:45:24 +0100

    complete get users and user detail resources

--- api.py
+++ api.py
@@ -6,6 +6,14 @@ from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+api = Api(app)
+userFields = {
+  'id': fields.Integer,
+  'fname': fields.String,
+  'lname': fields.String,
+  'email': fields.String
+}
+
 
 class UserModel(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +23,40 @@ class UserModel(db.Model):
   
   def __repr__(self):
     return f"User(fname = {self.fname}, lname = {self.lname}, email = {self.email})"
+  
+class Users(Resource):
+  @marshal_with(userFields)
+  def get(self):
+    users = UserModel.query.all()
+    return users
+  
+  @marshal_with(userFields)
+  def post(self):
+    args = userargs.parse_args()
+    user = UserModel(fname=args["fname"], lname=args["lname"], email=args["email"])
+    db.session.add(user)
+    db.session.commit()
+    users = UserModel.query.all()
+    return users, 201
+  
+
+class User(Resource):
+  @marshal_with(userFields)
+  def get(self, id):
+    user = UserModel.query.filter_by(id=id).first()
+    if not user:
+      abort(404, "User not found")
+    return user
+
+
+
+api.add_resource(Users, '/api/users/')
+api.add_resource(User, '/api/users/<int:id>')
+
+userargs = reqparse.RequestParser()
+userargs.add_argument('fname', type=str, required=True, help='Firstname is required')
+userargs.add_argument('lname', type=str, required=True, help='Lastname is required')
+userargs.add_argument('email', type=str, required=True, help='Email is required')
 
 @app.route("/")
 def home():
